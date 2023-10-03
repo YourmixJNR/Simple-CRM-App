158dd2a42af920d262553521baccd18594754e41
Michael Victor <radmakson@gmail.com>
2023-10-03 16:19:22 +0100

    init

--- js/dashboard.js
+++ js/dashboard.js
@@ -49,32 +49,3 @@
   })
 }
 )()
-
-
-// Get a reference to the canvas element where your Chart.js chart is rendered.
-const canvas = document.getElementById('myChart');
-
-// Get a reference to the download button.
-const downloadButton = document.getElementById('downloadButton');
-
-// Function to trigger the download.
-function downloadChart() {
-  // Create an "off-screen" canvas element to convert the chart to an image.
-  const tempCanvas = document.createElement('canvas');
-  const tempContext = tempCanvas.getContext('2d');
-  tempCanvas.width = canvas.width;
-  tempCanvas.height = canvas.height;
-  tempContext.drawImage(canvas, 0, 0);
-
-  // Create a data URL for the canvas content (PNG format).
-  const dataURL = tempCanvas.toDataURL('image/png');
-
-  // Create a download link.
-  const link = document.createElement('a');
-  link.href = dataURL;
-  link.download = 'chart.png';
-  link.click();
-}
-
-// Add a click event listener to the download button.
-downloadButton.addEventListener('click', downloadChart);
