dfe48bcd8a8d8d63c8dabe775aaa313e89e495e6
Michael Victor <radmakson@gmail.com>
2023-10-04 13:36:29 +0100

    open database

---
+++ js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Open Database
+    const request = indexedDB.open('customermanager', 1);
+
+    // Declare Variable
+    let db;
+
+    request.onupgradeneeded = function(event) {
+        db = event.target.result;
+
+        if(!db.objectStoreNames.contain(customers)){
+            const objectStore = db.createObjectStore('customers', {keyPath: "id", autoIncrement: true});
+
+            // Create Index for Name
+            objectStore.createIndex('name', 'name', {unique: false});
+        }
+    };
+
+    // Success
+    request.onsuccess = function(event){
+        db = event.target.result;
+        console.log('Success: Opened Database');
+    };
+
+    // Error
+    request.onerror = function(event){
+        console.log('Error: ' `${event.target.errorCode}`);
+    }
+    
+});
